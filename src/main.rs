3d4c0a1e7a303cf8ca85f9d730cd3c198537bdf1
Guillaume Pinot <texitoi@texitoi.eu>
2018-09-16 13:03:52 +0200

    cleanups

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
 extern crate stm32f103xx_hal as hal;
 extern crate embedded_graphics;
+extern crate embedded_hal;
 
 use hal::prelude::*;
 use hal::spi::{Mode, Spi, Phase::*, Polarity::*};
@@ -14,6 +15,7 @@ use embedded_graphics::coord::Coord;
 use embedded_graphics::fonts::Font6x8;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{Circle, Line};
+use core::marker::PhantomData;
 
 #[entry]
 fn main() -> ! {
@@ -29,7 +31,7 @@ fn main() -> ! {
     let sck = gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh);
     let miso = gpiob.pb14;
     let mosi = gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh);
-    let spi = Spi::spi2(
+    let mut spi = Spi::spi2(
         dp.SPI2,
         (sck, miso, mosi),
         Mode { polarity: IdleLow, phase: CaptureOnFirstTransition },
@@ -38,45 +40,16 @@ fn main() -> ! {
         &mut rcc.apb1,
     );
     
-    let mut il3820 = Il3820 {
-        spi,
-        nss: gpiob.pb12.into_push_pull_output(&mut gpiob.crh),
-        dc: gpioa.pa8.into_push_pull_output(&mut gpioa.crh),
-        rst: gpioa.pa9.into_push_pull_output(&mut gpioa.crh),
-        busy: gpioa.pa10.into_floating_input(&mut gpioa.crh),
-    };
+    let mut il3820 = Il3820::new(
+        &mut spi,
+        gpiob.pb12.into_push_pull_output(&mut gpiob.crh),
+        gpioa.pa8.into_push_pull_output(&mut gpioa.crh),
+        gpioa.pa9.into_push_pull_output(&mut gpioa.crh),
+        gpioa.pa10.into_floating_input(&mut gpioa.crh),
+        &mut delay,
+    ).unwrap();
 
     loop {
-        il3820.rst.set_low();
-        delay.delay_ms(200u16);
-        il3820.rst.set_high();
-        delay.delay_ms(200u16);
-        il3820.nss.set_low();
-
-        il3820.cmd_with_data(0x01, &[0x27, 0x01, 0x00]).unwrap();//GDOControl (screen width)
-        il3820.cmd_with_data(0x0c, &[0xd7, 0xd6, 0x9d]).unwrap();// softstart
-        il3820.cmd_with_data(0x2C, &[0xA8]).unwrap();//VCOMVol
-        il3820.cmd_with_data(0x3a, &[0x1a]).unwrap();//dummy line
-        il3820.cmd_with_data(0x3b, &[0x08]).unwrap();//Gate Time
-        il3820.cmd_with_data(0x11, &[3]).unwrap();//ram data entry mode
-
-        // set ram area
-        il3820.cmd_with_data(0x44, &[0, 127 / 8]).unwrap();// x
-        il3820.cmd_with_data(0x45, &[0, 0, 39, 1]).unwrap();// y
-
-        // set ram ptr
-        il3820.cmd_with_data(0x4e, &[0]).unwrap();// x
-        il3820.cmd_with_data(0x4f, &[0, 0]).unwrap();// y
-
-        // set LUT
-        il3820.cmd_with_data(0x32, &LUT_FULL).unwrap();
-
-        // power on
-        il3820.cmd_with_data(0x22, &[0xc3]).unwrap();
-        il3820.cmd(0x20).unwrap();
-
-        // set ram with funky data
-        il3820.cmd(0x24).unwrap();
         let mut display = Display::default();
         display.draw(
             Circle::new(Coord::new(64, 64), 64)
@@ -99,23 +72,14 @@ fn main() -> ! {
                 .translate(Coord::new(5, 50))
                 .into_iter(),
         );
-        il3820.write_data(&display.0).unwrap();
+        il3820.set_display(&mut spi, &display).unwrap();
+        il3820.update(&mut spi).unwrap();
 
-        // update full
-        il3820.cmd_with_data(0x22, &[0xc4]).unwrap();
-        il3820.cmd(0x20).unwrap();
-        il3820.cmd(0xff).unwrap();
-
-        // power off
-        il3820.cmd_with_data(0x22, &[0xc3]).unwrap();
-        il3820.cmd(0x20).unwrap();
-
-        il3820.nss.set_high();
         delay.delay_ms(1_000u16);
     }
 }
 
-struct Display([u8; 296 * 128 / 8]);
+pub struct Display([u8; 296 * 128 / 8]);
 impl Default for Display {
     fn default() -> Self {
         Display([0xff; 296 * 128 / 8])
@@ -145,27 +109,99 @@ static LUT_FULL: [u8; 30] = [
     0x00, 0x00, 0x00, 0x00, 0xF8, 0xB4, 0x13, 0x51, 
     0x35, 0x51, 0x51, 0x19, 0x01, 0x00
 ];
-struct Il3820 {
-    spi: hal::spi::Spi<hal::stm32f103xx::SPI2, (hal::gpio::gpiob::PB13<hal::gpio::Alternate<hal::gpio::PushPull>>, hal::gpio::gpiob::PB14<hal::gpio::Input<hal::gpio::Floating>>, hal::gpio::gpiob::PB15<hal::gpio::Alternate<hal::gpio::PushPull>>)>,
-    nss: hal::gpio::gpiob::PB12<hal::gpio::Output<hal::gpio::PushPull>>,
-    dc: hal::gpio::gpioa::PA8<hal::gpio::Output<hal::gpio::PushPull>>,
-    rst: hal::gpio::gpioa::PA9<hal::gpio::Output<hal::gpio::PushPull>>,
-    busy: hal::gpio::gpioa::PA10<hal::gpio::Input<hal::gpio::Floating>>,
+
+
+pub struct Il3820<S, N, D, R, B> {
+    _spi: PhantomData<S>,
+    nss: N,
+    dc: D,
+    rst: R,
+    busy: B,
 }
-impl Il3820 {
-    fn cmd(&mut self, c: u8) -> Result<(), hal::spi::Error> {
+impl<S, N, D, R, B> Il3820<S, N, D, R, B>
+    where
+    S: embedded_hal::blocking::spi::Write<u8>,
+    N: embedded_hal::digital::OutputPin,
+    D: embedded_hal::digital::OutputPin,
+    R: embedded_hal::digital::OutputPin,
+    B: embedded_hal::digital::InputPin,
+{
+    pub fn new<DELAY: embedded_hal::blocking::delay::DelayMs<u8>>(
+        spi: &mut S,
+        nss: N,
+        dc: D,
+        rst: R,
+        busy: B,
+        delay: &mut DELAY
+    ) -> Result<Self, S::Error> {
+        let mut res = Self {
+            _spi: PhantomData::default(),
+            nss,
+            dc,
+            rst,
+            busy,
+        };
+
+        // reset
+        res.rst.set_low();
+        delay.delay_ms(200u8);
+        res.rst.set_high();
+        delay.delay_ms(200u8);
+
+        res.cmd_with_data(spi, 0x01, &[0x27, 0x01, 0x00])?;//GDOControl (screen width)
+        res.cmd_with_data(spi, 0x0c, &[0xd7, 0xd6, 0x9d])?;// softstart
+        res.cmd_with_data(spi, 0x2C, &[0xA8])?;//VCOMVol
+        res.cmd_with_data(spi, 0x3a, &[0x1a])?;//dummy line
+        res.cmd_with_data(spi, 0x3b, &[0x08])?;//Gate Time
+        res.cmd_with_data(spi, 0x32, &LUT_FULL)?;// set LUT
+
+        // power on
+        res.cmd_with_data(spi, 0x22, &[0xc3])?;
+        res.cmd(spi, 0x20)?;
+
+        res.cmd_with_data(spi, 0x11, &[3])?;//ram data entry mode
+
+        // set ram area
+        res.cmd_with_data(spi, 0x44, &[0, 127 / 8])?;// x
+        res.cmd_with_data(spi, 0x45, &[0, 0, 39, 1])?;// y
+
+        // set ram ptr
+        res.cmd_with_data(spi, 0x4e, &[0])?;// x
+        res.cmd_with_data(spi, 0x4f, &[0, 0])?;// y
+
+        Ok(res)
+    }
+    pub fn set_display(&mut self, spi: &mut S, display: &Display) -> Result<(), S::Error> {
+        // set ram
+        self.cmd_with_data(spi, 0x24, &display.0)
+    }
+    pub fn update(&mut self, spi: &mut S) -> Result<(), S::Error> {
+        self.cmd_with_data(spi, 0x22, &[0xc4])?;
+        self.cmd(spi, 0x20)?;
+        self.cmd(spi, 0xff)
+    }
+    pub fn power_off(&mut self, spi: &mut S) -> Result<(), S::Error> {
+        self.cmd_with_data(spi, 0x22, &[0xc3])?;
+        self.cmd(spi, 0x20)
+    }
+    fn cmd(&mut self, spi: &mut S, c: u8) -> Result<(), S::Error> {
+        self.nss.set_low();
         while self.busy.is_high() {}
         self.dc.set_low();
-        self.spi.write(&[c])?;
+        spi.write(&[c])?;
+        self.nss.set_high();
         Ok(())
     }
-    fn write_data(&mut self, data: &[u8]) -> Result<(), hal::spi::Error> {
+    fn write_data(&mut self, spi: &mut S, data: &[u8]) -> Result<(), S::Error> {
+        self.nss.set_low();
         self.dc.set_high();
-        self.spi.write(data)
+        spi.write(data)?;
+        self.nss.set_high();
+        Ok(())
     }
-    fn cmd_with_data(&mut self, c: u8, data: &[u8]) -> Result<(), hal::spi::Error> {
-        self.cmd(c)?;
-        self.write_data(data)
+    fn cmd_with_data(&mut self, spi: &mut S, c: u8, data: &[u8]) -> Result<(), S::Error> {
+        self.cmd(spi, c)?;
+        self.write_data(spi, data)
     }
 }
 
