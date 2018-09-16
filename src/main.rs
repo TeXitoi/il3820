d755f3048c43b0e3b1a6c26b71a0f67dfa5a20e0
Guillaume Pinot <texitoi@texitoi.eu>
2018-09-16 23:15:58 +0200

    partial update

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ extern crate panic_semihosting;
 extern crate stm32f103xx_hal as hal;
 extern crate embedded_graphics;
 extern crate embedded_hal;
-extern crate heapless;
 
 use hal::prelude::*;
 use hal::spi::{Mode, Spi, Phase::*, Polarity::*};
@@ -17,8 +16,6 @@ use embedded_graphics::fonts::Font6x8;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{Circle, Line};
 use core::marker::PhantomData;
-use heapless::{String, consts::*};
-use core::fmt::Write;
 
 #[entry]
 fn entry() -> ! { main() }
@@ -51,18 +48,12 @@ fn main() -> ! {
         gpioa.pa9.into_push_pull_output(&mut gpioa.crh),
         gpioa.pa10.into_floating_input(&mut gpioa.crh),
         &mut delay,
-    ).unwrap();
+    );
+    il3820.clear(&mut spi).unwrap();
 
     let mut i = 0;
     loop {
         i += 1;
-        let mut s = String::<U16>::new();
-        //write!(s, "Hello world {}", i).unwrap();
-        if i%2 == 0 {
-            s.push_str("0").unwrap();
-        } else {
-            s.push_str("1").unwrap();
-        }
         let mut display = Display::default();
         display.draw(
             Circle::new(Coord::new(64, 64), 64)
@@ -80,16 +71,16 @@ fn main() -> ! {
                 .into_iter(),
         );
         display.draw(
-            Font6x8::render_str(&s)
+            Font6x8::render_str("Hello World!")
                 .with_stroke(Some(1u8.into()))
-                .translate(Coord::new(5, 50))
+                .translate(Coord::new(5, 50 + i))
                 .into_iter(),
         );
-        il3820.set_display(&mut spi, &display).unwrap();
+        if i % 10 == 9 { il3820.set_full(); }
         il3820.set_display(&mut spi, &display).unwrap();
         il3820.update(&mut spi).unwrap();
-
-        delay.delay_ms(1_000u16);
+        il3820.set_partial();
+        //delay.delay_ms(1_000u16);
     }
 }
 
@@ -137,7 +128,8 @@ pub struct Il3820<S, N, D, R, B> {
     dc: D,
     rst: R,
     busy: B,
-    full: bool,
+    partial: bool,
+    is_inited: bool,
 }
 impl<S, N, D, R, B> Il3820<S, N, D, R, B>
     where
@@ -148,72 +140,107 @@ impl<S, N, D, R, B> Il3820<S, N, D, R, B>
     B: embedded_hal::digital::InputPin,
 {
     pub fn new<DELAY: embedded_hal::blocking::delay::DelayMs<u8>>(
-        spi: &mut S,
+        _spi: &mut S,
         nss: N,
         dc: D,
         rst: R,
         busy: B,
         delay: &mut DELAY
-    ) -> Result<Self, S::Error> {
+    ) -> Self {
         let mut res = Self {
             _spi: PhantomData::default(),
             nss,
             dc,
             rst,
             busy,
-            full: true,
+            partial: true,
+            is_inited: false,
         };
-
-        // reset
-        res.rst.set_low();
-        delay.delay_ms(200u8);
-        res.rst.set_high();
-        delay.delay_ms(200u8);
-
-        res.cmd_with_data(spi, 0x01, &[0x27, 0x01, 0x00])?;//GDOControl (screen width)
-        res.cmd_with_data(spi, 0x0c, &[0xd7, 0xd6, 0x9d])?;// softstart
-        res.cmd_with_data(spi, 0x2C, &[0xA8])?;//VCOMVol
-        res.cmd_with_data(spi, 0x3a, &[0x1a])?;//dummy line
-        res.cmd_with_data(spi, 0x3b, &[0x08])?;//Gate Time
-
-        if res.full {
-            res.cmd_with_data(spi, 0x32, &LUT_FULL)?;// set LUT
-        } else {
-            res.cmd_with_data(spi, 0x32, &LUT_PART)?;// set LUT
+        res.reset(delay);
+        res
+    }
+    pub fn reset<DELAY: embedded_hal::blocking::delay::DelayMs<u8>>(
+        &mut self,
+        delay: &mut DELAY
+    ) {
+        self.rst.set_low();
+        delay.delay_ms(1u8);
+        self.rst.set_high();
+        delay.delay_ms(1u8);
+    }
+    pub fn set_partial(&mut self) {
+        if !self.partial {
+            self.partial = true;
+            self.is_inited = false;
         }
-
-        // power on
-        res.cmd_with_data(spi, 0x22, &[0xc3])?;
-        res.cmd(spi, 0x20)?;
-
-        res.cmd_with_data(spi, 0x11, &[3])?;//ram data entry mode
-
-        // set ram area
-        res.cmd_with_data(spi, 0x44, &[0, 127 / 8])?;// x
-        res.cmd_with_data(spi, 0x45, &[0, 0, 39, 1])?;// y
-
-        // set ram ptr
-        res.cmd_with_data(spi, 0x4e, &[0])?;// x
-        res.cmd_with_data(spi, 0x4f, &[0, 0])?;// y
-
-        Ok(res)
+    }
+    pub fn set_full(&mut self) {
+        if self.partial {
+            self.partial = false;
+            self.is_inited = false;
+        }
+    }
+    pub fn clear(&mut self, spi: &mut S) -> Result<(), S::Error> {
+        let partial = self.partial;
+        self.partial = false;
+        self.init(spi)?;
+        if !self.is_inited { self.init(spi)?; }
+        self.cmd(spi, 0x24)?;
+        for _ in 0..128 / 8 * 296 {
+            self.write_data(spi, &[0xFF])?;
+        }
+        self.update(spi)?;
+        if partial != self.partial {
+            self.partial = partial;
+            self.is_inited = false;
+        }
+        Ok(())
     }
     pub fn set_display(&mut self, spi: &mut S, display: &Display) -> Result<(), S::Error> {
-        // set ram
+        if !self.is_inited { self.init(spi)?; }
         self.cmd_with_data(spi, 0x24, &display.0)
     }
     pub fn update(&mut self, spi: &mut S) -> Result<(), S::Error> {
-        if self.full {
-            self.cmd_with_data(spi, 0x22, &[0xc4])?;// full
-        } else {
-            self.cmd_with_data(spi, 0x22, &[0x04])?;// partial
-        }
+        if !self.is_inited { self.init(spi)?; }
+        self.cmd_with_data(spi, 0x22, &[0xc4])?;
         self.cmd(spi, 0x20)?;
         self.cmd(spi, 0xff)
     }
     pub fn power_off(&mut self, spi: &mut S) -> Result<(), S::Error> {
         self.cmd_with_data(spi, 0x22, &[0xc3])?;
-        self.cmd(spi, 0x20)
+        self.cmd(spi, 0x20)?;
+        self.is_inited = false;
+        Ok(())
+    }
+    fn init(&mut self, spi: &mut S) -> Result<(), S::Error> {
+        self.cmd_with_data(spi, 0x01, &[0x27, 0x01, 0x00])?;//GDOControl (screen width)
+        self.cmd_with_data(spi, 0x0c, &[0xd7, 0xd6, 0x9d])?;// softstart
+        self.cmd_with_data(spi, 0x2c, &[0xa8])?;//VCOMVol
+        self.cmd_with_data(spi, 0x3a, &[0x1a])?;//dummy line
+        self.cmd_with_data(spi, 0x3b, &[0x08])?;//Gate Time
+        self.cmd_with_data(spi, 0x11, &[3])?;//ram data entry mode
+
+        // set ram area
+        self.cmd_with_data(spi, 0x44, &[0, 127 / 8])?;// x
+        self.cmd_with_data(spi, 0x45, &[0, 0, 39, 1])?;// y
+
+        // set ram ptr
+        self.cmd_with_data(spi, 0x4e, &[0])?;// x
+        self.cmd_with_data(spi, 0x4f, &[0, 0])?;// y
+
+        if self.partial {
+            self.cmd_with_data(spi, 0x32, &LUT_PART)?;
+        } else {
+            self.cmd_with_data(spi, 0x32, &LUT_FULL)?;
+        }
+
+        // power on
+        self.cmd_with_data(spi, 0x22, &[0xc3])?;
+        self.cmd(spi, 0x20)?;
+
+        self.is_inited = true;
+
+        Ok(())
     }
     fn cmd(&mut self, spi: &mut S, c: u8) -> Result<(), S::Error> {
         self.nss.set_low();
